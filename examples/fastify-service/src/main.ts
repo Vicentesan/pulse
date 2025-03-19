29b4793892400262c8545bc96e75981b2b0d7d8e
Vicentesan <vikom.sanchez@gmail.com>
2025-03-19 12:59:36 -0300

    chore: remove examples

--- examples/fastify-service/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { startServer } from './http/server'
-
-async function main() {
-  startServer()
-}
-
-main().catch((err) => {
-  console.error('Error initializing Pulse Fastify Service', err)
-})
